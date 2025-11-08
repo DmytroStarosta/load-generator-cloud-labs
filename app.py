3e9a2e4aceac3b555cb1addf0168bd3501cba350
DmytroStarosta <151664402+DmytroStarosta@users.noreply.github.com>
2025-11-08 23:18:34 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import asyncio
+from project.generator_load import start_generator_load
+from my import MY_URL
+
+URL = MY_URL
+
+if __name__ == "__main__":
+    while True:
+        asyncio.run(start_generator_load(URL, total=300, concurrency=50))
